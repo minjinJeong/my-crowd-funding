ba482c9164db49c0516af260189e52c657709851
minjinJeong <alswls4390@naver.com>
2024-01-26 23:30:36 +0900

    Usecase Configuration 설정

--- src/main/java/com/flab/funding/infrastructure/config/SpringConfiguration.java
+++ src/main/java/com/flab/funding/infrastructure/config/SpringConfiguration.java
@@ -1,7 +1,13 @@
 package com.flab.funding.infrastructure.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
+@ComponentScan(
+        basePackages = "com.flab.funding",
+        includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = UseCase.class)
+)
 public class SpringConfiguration {
 }
