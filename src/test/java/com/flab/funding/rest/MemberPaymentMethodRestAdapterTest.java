72e664808d854797d90af2db3ac37627545175ab
minjinJeong <alswls4390@naver.com>
2024-03-16 20:54:29 +0900

    결제수단, 배송지 테스트에 모킹 적용

--- src/test/java/com/flab/funding/rest/MemberPaymentMethodRestAdapterTest.java
+++ src/test/java/com/flab/funding/rest/MemberPaymentMethodRestAdapterTest.java
@@ -1,38 +1,34 @@
 package com.flab.funding.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.flab.funding.domain.model.MemberGender;
-import com.flab.funding.domain.model.MemberLinkType;
+import com.flab.funding.domain.model.Member;
+import com.flab.funding.domain.model.PaymentMethod;
+import com.flab.funding.domain.service.MemberPaymentMethodService;
 import com.flab.funding.infrastructure.adapters.input.data.request.MemberPaymentMethodRegisterRequest;
-import com.flab.funding.infrastructure.adapters.input.data.request.MemberRegisterRequest;
-import com.flab.funding.infrastructure.adapters.input.data.response.MemberRegisterResponse;
-import org.junit.jupiter.api.BeforeEach;
+import com.flab.funding.infrastructure.adapters.input.rest.MemberPaymentMethodRestAdapter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(RestDocumentationExtension.class)
-@SpringBootTest
-@Transactional
-@AutoConfigureMockMvc
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@WebMvcTest(MemberPaymentMethodRestAdapter.class)
+@AutoConfigureRestDocs
 public class MemberPaymentMethodRestAdapterTest {
 
     @Autowired
@@ -41,54 +37,28 @@ public class MemberPaymentMethodRestAdapterTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private String userKey;
-
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-
-        registerMember();
-    }
-
-    private void registerMember() throws Exception {
-        // given
-        MemberRegisterRequest request = MemberRegisterRequest.builder()
-                .linkType(MemberLinkType.NONE)
-                .email("PaymentMethod@gmail.com")
-                .userName("홍길순")
-                .nickName("테스터")
-                .phoneNumber("010-1111-2222")
-                .gender(MemberGender.FEMALE)
-                .birthday(LocalDate.of(1998,1,30))
-                .password("")
-                .build();
-
-        // when
-        // then
-        ResultActions resultActions = this.mockMvc.perform(post("/members")
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-        MemberRegisterResponse response = objectMapper.readValue(
-                resultActions.andReturn().getResponse().getContentAsString(),
-                MemberRegisterResponse.class);
-
-        userKey = response.getUserKey();
-    }
-
+    @MockBean
+    private MemberPaymentMethodService memberPaymentMethodService;
 
     @Test
     public void registerPaymentMethod() throws Exception {
         // given
         MemberPaymentMethodRegisterRequest request = MemberPaymentMethodRegisterRequest.builder()
-                .userKey("1L")
+                .userKey("MM-0001")
                 .isDefault(true)
-                .paymentNumber("3565 43")
+                .paymentNumber("3565-43")
                 .build();
+
+        PaymentMethod response = PaymentMethod.builder()
+                .paymentMethodKey("PM-0001")
+                .member(Member.builder().userKey("MM-0001").build())
+                .isDefault(true)
+                .paymentNumber("3565-43")
+                .createdAt(LocalDateTime.of(2024, 3, 15, 10, 9))
+                .updatedAt(LocalDateTime.of(2024, 3, 15, 10, 9))
+                .build();
+
+        given(memberPaymentMethodService.registerPaymentMethod(any(request.toPaymentMethod().getClass()))).willReturn(response);
 
         //when
 
